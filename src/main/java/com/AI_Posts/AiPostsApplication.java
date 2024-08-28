124348fa2bb8b0931dc0e05618b29c7428f08e12
Vtormacs <macskids98play@hotmail.com>
2024-08-28 11:06:58 -0300

    Criação

---
+++ src/main/java/com/AI_Posts/AiPostsApplication.java
@@ -0,0 +1,13 @@
+package com.AI_Posts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPostsApplication.class, args);
+	}
+
+}
