3c18cf76fb34e372666114460e9b9044af9243ec
Vtormacs <macskids98play@hotmail.com>
2024-09-04 11:56:35 -0300

    teste IA

--- src/main/java/com/AI_Posts/Controller/AI/GeminiController.java
+++ src/main/java/com/AI_Posts/Controller/AI/GeminiController.java
@@ -1,24 +1,20 @@
 package com.AI_Posts.Controller.AI;
 
 import com.AI_Posts.Service.AI.GeminiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/ia")
 public class GeminiController {
 
-    private final GeminiService geminiService;
-
-    public GeminiController(GeminiService geminiService) {
-        this.geminiService = geminiService;
-    }
-
-    @GetMapping("/generate")
-    public String generateContent() {
-        return geminiService.generateContent();
-    }
+    @Autowired
+    private GeminiService geminiService;
 
     @GetMapping("/validate")
-    public String validateInput(@RequestParam String userInput) {
-        return geminiService.validadeAI(userInput);
+    public ResponseEntity<String> validateInput(@RequestParam String userInput) {
+
+        return ResponseEntity.ok(geminiService.validadeAI(userInput));
     }
 }
--- src/test/java/com/AI_Posts/Service/AI/GeminiServiceTest.java
+++ src/test/java/com/AI_Posts/Service/AI/GeminiServiceTest.java
@@ -1,14 +1,19 @@
 package com.AI_Posts.Service.AI;
 
+import com.AI_Posts.Controller.AI.GeminiController;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class GeminiServiceTest {
+
+    @Autowired
+    GeminiController geminiController;
 
     @Autowired
     GeminiService geminiService;
@@ -32,4 +37,5 @@ class GeminiServiceTest {
 
         System.out.println(retorno);
     }
+
 }
